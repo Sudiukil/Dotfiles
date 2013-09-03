084b423a5290532385af5e3e54955f68071b4e0d
Sudiukil <sudiukil@gmx.fr>
2013-09-03 17:36:29 +0200

    Updated py3status MOC module

--- Config files/i3wm/py3status/moc.py
+++ Config files/i3wm/py3status/moc.py
@@ -1,13 +1,12 @@
 import os
 
-moduleName = "Py3status MOC module 0.1"
+moduleName = "Py3status MOC module 0.2"
 
 class Py3status:
 	def mainMethod(self, json, i3status_config):
 		state = os.popen("mocp -Q %state").readline()[:-1]
 		if state == "PLAY":
-			display = state + ": " + os.popen("mocp -Q %title").readline()[:-1]
-			output = (0, {"full_text" : display, "name" : moduleName})
+			display = "MOC: " + state + ": " + os.popen("mocp -Q %title").readline()[:-1]
 		else:
-			output = (0, {"full_text" : state, "name" : moduleName})
-		return output
+			display = "MOC: " + state
+		return (0, {"full_text" : display, "name" : moduleName})
