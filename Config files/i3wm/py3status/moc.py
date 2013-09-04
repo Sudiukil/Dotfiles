255618c276c712e90b1ca1f4cd2286b179cccbe4
Sudiukil <sudiukil@gmx.fr>
2013-09-04 13:28:28 +0200

    Some updates & py3status public ip module added

--- Config files/i3wm/py3status/moc.py
+++ Config files/i3wm/py3status/moc.py
@@ -10,7 +10,7 @@ class Py3status:
 			currentTime = os.popen("mocp -Q %ct").readline()[:-1]
 			timeLeft = os.popen("mocp -Q %tl").readline()[:-1]
 			totalTime = os.popen("mocp -Q %tt").readline()[:-1]
-			display = "MOC: " + state + ' ' + title + ' ' + currentTime + '/' + timeLeft + ' [' + totalTime + ']'
+			display = "MOC: " + state + ': ' + title + ' ' + currentTime + '/' + timeLeft + ' [' + totalTime + ']'
 		else:
 			display = "MOC: " + state
 		return (0, {"full_text" : display, "name" : moduleName})
---
+++ Config files/i3wm/py3status/pubip.py
@@ -0,0 +1,9 @@
+import os
+
+moduleName = "py3status public ip module 0.1"
+
+class Py3status:
+	def mainMethod(self, json, i3status_config):
+		pubIp = os.popen("sleep 300 && curl ifconfig.me").readline()[:-1]
+		display = "IP Publique: " + pubIp
+		return (1, {"full_text" : display, "name" : moduleName})
