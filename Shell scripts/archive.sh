5273b9b6b6a64d7523599a0bcf91057d2b0880a6
Sudiukil <sudiukil@gmx.fr>
2017-04-27 15:37:43 +0200

    Updated archive.sh to handle more than one archive for extraction

--- Shell scripts/archive.sh
+++ Shell scripts/archive.sh
@@ -3,27 +3,34 @@
 # extract or create various type of archive
 
 print_usage() {
-echo "Usage:"
-echo "${0##*/} -x <archive>\t\tExtract archive"
-echo "${0##*/} -c <file> <archive>\tCreate archive (according to archive extension provided)"
-echo "${0##*/} -h\t\t\tPrint this help message"
-echo "Warning: only works for one archive and one file at a time"
+	echo "Usage:"
+	echo "${0##*/} -x <archive>\t\tExtract archive"
+	echo "${0##*/} -c <file> <archive>\tCreate archive (according to archive extension provided)"
+	echo "${0##*/} -h\t\t\tPrint this help message"
+	echo "Warning: only works for one archive and one file at a time for compression (-c)"
 }
 
 case $1 in
 	-x|x)
-		if [ -f "$2" ]
+		if [ $# -ge 2 ]
 		then
-			case $2 in
-				*.tar|*.tar.*|*.tgz) tar xvf "$2";;
-				*.zip) unzip "$2";;
-				*.rar) unrar x "$2";;
-				*.jar) jar xvf "$2";;
-				*.7z) 7z x "$2";;
-				*)
-					echo "Error: $2: bad file type (compatible types: tar(.*), zip, rar, jar, 7z)"
-					exit 1;;
-			esac
+			for arg in $*
+			do
+				if [ $arg = $1 ]
+				then
+					continue
+				fi
+				case $arg in
+					*.tar|*.tar.*|*.tgz) tar xvf "$arg";;
+					*.zip) unzip "$arg";;
+					*.rar) unrar x "$arg";;
+					*.jar) jar xvf "$arg";;
+					*.7z) 7z x "$arg";;
+					*)
+						echo "Error: $arg: bad file type (compatible types: tar(.*), zip, rar, jar, 7z)"
+						exit 1;;
+				esac
+			done
 		else
 			echo "Error: file $2 not found or not a regular file"
 			exit 1
