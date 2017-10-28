bbedd15e404a7fd809f4e9ab8793df5a7bfdae28
Sudiukil <sudiukil@gmx.fr>
2017-10-28 11:01:25 +0200

    Global update + update.sh

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cp ~/.aliases aliases
+cp ~/.config/dunst/dunstrc dunstrc
+cp ~/.gitconfig gitconfig
+cp ~/.i3/config i3wm
+cp ~/.config/ncmpcpp/ncmpcpp ncmpcpp
+cp -r ~/.config/ranger/rc.conf ranger-rc.conf
+cp ~/.vimrc vim
+cp ~/.Xresources Xresources
+cp ~/.zshrc zshrc
+
+exit 0
