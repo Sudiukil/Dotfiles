515be0708496c3bd0e153ed3b261736628e402bb
Sudiukil <sudiukil@gmx.fr>
2018-07-04 19:30:37 +0200

    Update

--- update.sh
+++ update.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 
-cp ~/.aliases aliases
-cp ~/.config/dunst/dunstrc dunstrc
-cp ~/.gitconfig gitconfig
-cp ~/.config/i3/config i3wm
-cp -r ~/.config/ranger/rc.conf ranger-rc.conf
-cp ~/.vimrc vim
+# CLI env
 cp ~/.Xresources Xresources
 cp ~/.zshrc zshrc
+cp ~/.zprofile zprofile
+cp ~/.aliases aliases
 cp ~/.shellfuncs/*.sh zsh-functions/
+
+# Graphical env
+cp ~/.config/i3/config i3wm
+cp ~/.config/dunst/dunstrc dunstrc
+
+# Various tools
+cp ~/.gitconfig gitconfig
+cp ~/.vimrc vim
+cp -r ~/.config/ranger/rc.conf ranger-rc.conf
+
+# Multimedia
 cp ~/.config/mpd/mpd.conf mpd
 cp ~/.config/ncmpcpp/config ncmpcpp
 
