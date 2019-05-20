687997cbb31d99acdda8e220e8412bfda411a467
Quentin Sonrel <sudiukil@gmx.fr>
2019-05-20 08:27:09 +0200

    Fixed update script

--- update.sh
+++ update.sh
@@ -7,8 +7,8 @@ cp ~/.zprofile zprofile
 cp ~/.xinitrc xinitrc
 cp ~/.envrc envrc
 cp ~/.aliases aliases
-rm -r zsh-functions
-cp ~/.functions/ zsh-functions/
+rm -rf zsh-functions
+cp -r ~/.functions/ zsh-functions/
 
 # Graphical env
 cp ~/.config/i3/config i3wm
@@ -22,15 +22,19 @@ cp -r ~/.config/ranger/rc.conf ranger-rc.conf
 
 # Vim plugins
 this_dir=$(pwd)
-cd ~/.vim/pack/plugins/start
+vim_plugins_dir="$HOME/.vim/pack/git-plugins/start/"
+
+cd $vim_plugins_dir
 [ -f list ] && rm list
+
 ls | while read i; do
   cd $i
   echo $(git config --get remote.origin.url) >> ../list
   cd ..
 done
+
 cd $this_dir
-mv ~/.vim/pack/plugins/start/list vim_plugins
+mv $vim_plugins_dir/list vim_plugins
 
 # Multimedia
 cp ~/.config/mpd/mpd.conf mpd
