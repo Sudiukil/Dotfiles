79538421b034edf5c0ce10fb2058be19e107abcc
Sudiukil <contact@sudiukil.dev>
2020-12-14 13:15:01 +0100

    Added deploy script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+ln -s $PWD/zshrc $HOME/.zshrc
+ln -s $PWD/aliases $HOME/.aliases
+ln -s $PWD/zsh-functions $HOME/.functions
+ln -s $PWD/envrc $HOME/.envrc
+ln -s $PWD/gitconfig $HOME/.gitconfig
+
+exit 0
