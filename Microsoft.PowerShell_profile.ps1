4eafa3e94c77d56a6b4ac8094e3024b1546857d6
Sudiukil <contact@sudiukil.dev>
2022-04-20 09:26:40 +0200

    Update

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,24 @@
+# Refresh PATH
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+
+# Exit on Ctrl+D
+Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
+
+# Starship SHELL
+Invoke-Expression (&starship init powershell)
+
+# Functions
+Function Stop-Docker {
+    docker stop $(docker ps -aq)
+    Get-Process *docker* | Stop-Process -Force
+    wsl -t docker-desktop
+    wsl -t docker-desktop-data
+}
+
+Function Start-Docker {
+    Write-Output "Starting Docker service..."
+    Start-Service com.docker.service
+    Start-Sleep -Seconds 5
+    Write-Output "Starting Docker Desktop (wait for GUI)..."
+    & "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+}
