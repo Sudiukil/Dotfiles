3c1addcac6c258d16539f9f9e874401b5eb637c4
Sudiukil <contact@sudiukil.dev>
2022-05-17 18:34:38 +0200

    Update

--- update.sh
+++ update.sh
@@ -10,7 +10,6 @@ git pull -q
 
 # Checkup
 mkdir -p $HOME/.config
-! [ -d "$USERPROFILE" ] && echo "ERROR: can't access Windows drive, aborting." && exit 1
 
 # Linux
 ln -sf $PWD/zshrc $HOME/.zshrc
@@ -18,23 +17,8 @@ ln -sf $PWD/zshenv $HOME/.zshenv
 ln -sf $PWD/aliases $HOME/.aliases
 ln -sf $PWD/functions $HOME/.functions
 ln -sf $PWD/gitconfig.linux $HOME/.gitconfig
-
-# Windows
-cp ./Microsoft.PowerShell_profile.ps1 $USERPROFILE/Documents/PowerShell/
-cp ./starship.toml $USERPROFILE/.config/
-cp ./gitconfig.windows $USERPROFILE/.gitconfig
-cp ./gitconfig.global $USERPROFILE/.gitconfig.global
-
-# Windows <=> WSL sync
-ln -sf $USERPROFILE/.config/starship.toml $HOME/.config/starship.toml
-ln -sf $USERPROFILE/.gitconfig.global $HOME/.gitconfig.global
-
-echo "\nWARNING: Some files need to be copied/linked manually (possibly as root):
-- wsl.conf -> /etc/wsl.conf
-- windows_terminal.json -> via Windows Terminal"
-
-echo "Reminder: some files were copied (not linked) and will need to be updated manually."
-echo "Also remember to correctly set WSLENV (Windows side, should include USERPROFILE and windir).\n"
+ln -sf $PWD/gitconfig.global $HOME/.gitconfig.global
+ln -sf $PWD/starship.toml $HOME/.config/starship.toml
 
 # Update to the repo
 git add -A
