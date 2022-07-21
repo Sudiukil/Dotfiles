efd61bc2aff1a628d311fefd991ff858eaf6bf9e
Sudiukil <contact@sudiukil.dev>
2022-07-21 15:23:35 +0200

    Update: Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,24 @@
 # Refresh PATH
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+function Update-Path {
+  $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+}
+Update-Path
+
+# Java shenanigans
+function Set-JavaVersion {
+  param(
+    [Parameter(Mandatory)]
+    [string] $Version
+  )
+
+  switch ($version) {
+    default { echo "Invalid Java version. Available versions: 8, 11" }
+    8 { $env:JAVA_HOME = "C:\\Program Files\\Amazon Corretto\\8" }
+    11 { $env:JAVA_HOME = "C:\\Program Files\\Amazon Corretto\\11" }
+  }
+
+  Update-Path
+}
 
 # Exit on Ctrl+D
 Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
