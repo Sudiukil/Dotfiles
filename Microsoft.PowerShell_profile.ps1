2100b722b53b607b596c5f16842061f203623540
Sudiukil <contact@sudiukil.dev>
2022-07-21 17:37:01 +0200

    Update: Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -7,14 +7,19 @@ Update-Path
 # Java shenanigans
 function Set-JavaVersion {
   param(
-    [Parameter(Mandatory)]
-    [string] $Version
+    [string] $Version,
+    [switch] $Persist
   )
 
   switch ($version) {
-    default { echo "Invalid Java version. Available versions: 8, 11" }
+    default { echo "Missing or invalid Java version. Available versions: 8, 11"; return }
     8 { $env:JAVA_HOME = "C:\\Program Files\\Amazon Corretto\\8" }
     11 { $env:JAVA_HOME = "C:\\Program Files\\Amazon Corretto\\11" }
+    17 { $env:JAVA_HOME = "C:\\Program Files\\Amazon Corretto\\17" }
+  }
+
+  if ($Persist) {
+    New-Item -ItemType SymbolicLink -Path "C:\Program Files\Amazon Corretto\default" -Target $env:JAVA_HOME -Force
   }
 
   Update-Path
