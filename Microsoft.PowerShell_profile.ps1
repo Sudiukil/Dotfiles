e51137c4bab371684d515d9be0e698b43b315ccb
Sudiukil <contact@sudiukil.dev>
2023-09-16 13:58:20 +0200

    Update: Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -28,7 +28,7 @@ function which {
 
 # Aliases
 Set-Alias -name grep -Value Select-String
-Set-Alias -name jq -Value jq.exe
+Set-Alias -name jq -Value jq-windows-amd64.exe
 
 # Starship Shell
 Invoke-Expression (&starship init powershell)
