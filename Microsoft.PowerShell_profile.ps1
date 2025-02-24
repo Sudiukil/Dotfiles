fe5f6edb6b79ae15084f9cc4f42e77b32cd18390
Sudiukil <contact@sudiukil.dev>
2025-02-24 19:52:27 +0100

    Update: Microsoft.PowerShell_profile.ps1, update.ps1, windows_terminal.json

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,9 @@
 # Refresh PATH
 function Update-Path {
   $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+  if ($env:Path -notlike "*$env:USERPROFILE\.bin*") {
+    $env:Path += ";" + "$env:USERPROFILE\.bin"
+  }
 }
 Update-Path
 
@@ -26,17 +29,30 @@ function which {
   (Get-Command $Command).path
 }
 
-# Start a Windows app
-function Start-WindowsApp {
-  param (
-    [Parameter(Mandatory)] [String] $AppName
+function sudo {
+  param(
+    [Parameter(Mandatory, ValueFromRemainingArguments = $true)] [string[]] $Command
   )
-  Start-Process shell:$("AppsFolder\" + (Get-AppxPackage | ?{$_.PackageFamilyName -like "*$AppName*"}).PackageFamilyName + "!App")
+
+  $Command = $Command -join " "
+  Start-Process -FilePath "pwsh" -ArgumentList "-NoProfile -Command $Command; Start-Sleep 5" -Verb RunAs
 }
 
 # Aliases
 Set-Alias -name grep -Value Select-String
 Set-Alias -name clip -Value Set-Clipboard
+
+# PowerShell parameter completion shim for the dotnet CLI
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+  param($wordToComplete, $commandAst, $cursorPosition)
+  dotnet complete --position $cursorPosition "$commandAst" | ForEach-Object {
+    [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+  }
+}
+
+# Dotnet Suggest
+dotnet suggest script powershell > $env:USERPROFILE\Documents\PowerShell\dotnet-suggest-shim.ps1
+. $env:USERPROFILE\Documents\PowerShell\dotnet-suggest-shim.ps1
 
 # Starship Shell
 Invoke-Expression (&starship init powershell)
