160e5512e8e4a3da80a9d3da690aa139bb311efa
Sudiukil <contact@sudiukil.dev>
2025-09-13 18:55:47 +0200

    Linux update

--- zsh/functions.sh
+++ zsh/functions.sh
@@ -5,13 +5,6 @@ cheatsheet() {
   curl "http://cheat.sh/$1"
 }
 
-# Grep processes, excluding the grep process with grep (grep!)
-pgrep() {
-  ps -axo "user,pid,%cpu,%mem,start,command" | head -n 1 # for the header
-  # shellcheck disable=SC2009
-  ps -axo "user,pid,%cpu,%mem,start,command" | grep -v grep | grep "$1" | trim # see aliases for trim
-}
-
 # Cut space-separated columns easily
 col() {
   tr -s ' ' | cut -d ' ' -f "$1"
