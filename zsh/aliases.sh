4635cf96c48b28943506165e9eec3644df5ed431
Sudiukil <contact@sudiukil.dev>
2025-11-28 14:51:42 +0100

    Cleaned up Linux aliases

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -7,24 +7,10 @@ alias rm="rm -I"
 alias wget="wget -q --show-progress"
 
 # Shell QoL
+alias cs="cheatsheet" # See functions
 alias renv="source ~/.zshenv && source ~/.zshrc"
 alias path='echo $PATH'
-alias syslog="tail -f /var/log/syslog"
-alias cdw="cd \$USERPROFILE"
-alias cs="cheatsheet" # See functions
 alias trim="cut -c \"1-\$COLUMNS\""
-
-# SSH
-alias stun="ssh -vTND 2222"
-alias unlock='bw get password "$USER@$HOST" | DISPLAY=:0 SSH_ASKPASS="$HOME/.ssh/askpass.sh" ssh-add "$HOME/.ssh/id_ed25519"'
-alias lock="keychain --agents \"ssh\" --clear && killall ssh"
-
-# Screen
-alias sl="screen -ls"
-alias ss="screen -S"
-alias sd="screen -dmS"
-alias sr="screen -r"
-alias sw="screen -wipe"
 
 # Dev stuff
 alias getrvm="\curl -sSL https://get.rvm.io | bash -s stable"
